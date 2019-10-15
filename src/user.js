bbd10b3b32b1ef3472bc11fd1c6d1684c8b717d1
marielysis <marielys11@yahoo.com>
2019-10-15 16:05:12 -0300

    User corriendo solo facebook y google

--- src/user.js
+++ src/user.js
@@ -140,39 +140,6 @@ const authFacebook = () => {
     authCuentaFacebook(provider);
 }
 
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-//authenticando con facebook
-  
-
-function authCuentaFacebook (provider) {
-     
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-        // ...
-     })
-     .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        console.log(errorCode)
-        var errorMessage = error.message;
-        console.log(errorMessage)
-        // The email of the user's account used.
-        var email = error.email;
-        console.log(email)
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        console.log(credential)
-        // ...
-     })
-    
-}
-
 //authenticando con facebook
 
 
